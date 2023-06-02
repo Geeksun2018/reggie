912550e7593bded4b0f3b8db290e8b5e0a879604
Geeksun2018 <995700257@qq.com>
2023-06-02 10:29:51 +0800

    静态资源

---
+++ src/test/java/com/geeksun/reggie/ReggieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geeksun.reggie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReggieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
